f73e1ea02c478d4d90a0c4808ec1616e1d3a6b54
Christoffer Øhrstrøm <chrisnave@gmail.com>
2020-02-24 15:27:47 +0900

    Logs following to tensorboard: Loss, epochs, total time, epoch time, batch time, and validation time.

--- app/tasks/train.py
+++ app/tasks/train.py
@@ -1,5 +1,7 @@
 from app.tasks.task import Task
-import torch
+import os
+import time
+from torch.utils.tensorboard import SummaryWriter
 
 class TrainTask(Task):
 
@@ -22,26 +24,56 @@ class TrainTask(Task):
         self._optimizer = optimizer
         self._stopping_criterion = stopping_criterion
 
+        working_directory = os.getcwd()
+        tensorboard_directory = os.path.join(working_directory, 'tb')
+        train_directory = os.path.join(tensorboard_directory, 'train')
+        val_directory = os.path.join(tensorboard_directory, 'val')
+        self._writer_train = SummaryWriter(log_dir=train_directory)
+        self._writer_val = SummaryWriter(log_dir=val_directory)
+
     def run(self):
-        epoch = 1
-        loss_val = self._validate()
+        time_start = time.time()
+        batch_count = 0
+        epoch = 0
+        loss_val = self._validate(batch_count)
         print(f'Epoch=0, loss_val={loss_val:0.4f}')
+        self._writer_train.add_scalar('epoch', epoch, batch_count)
         while not self._stopping_criterion.is_done(epoch, loss_val):
-            for batch_tokens, batch_actions in self._iterator_train:
-                _, batch_actions_lengths, _ = batch_actions
-                batch_log_probs = self._model(batch_tokens, batch_actions)
-                loss_train = self._loss(batch_log_probs, batch_actions_lengths)
-                self._optimize(loss_train)
-            loss_val = self._validate()
-            print(f'Epoch={epoch}, loss_train={loss_train:0.4f} loss_val={loss_val:0.4f}')
             epoch += 1
+            batch_count, loss_train = self._train_epoch(time_start, epoch, batch_count)
+            loss_val = self._validate(batch_count)
+            print(f'Epoch={epoch}, loss_train={loss_train:0.4f} loss_val={loss_val:0.4f}')
 
     def _optimize(self, loss):
         self._optimizer.zero_grad()
         loss.backward()
         self._optimizer.step()
 
-    def _validate(self):
+    def _train_epoch(self, time_start, epoch, batch_count):
+        time_epoch_start = time.time()
+        for batch in self._iterator_train:
+            batch_count += len(batch[1][1])
+            loss_train = self._train_batch(batch, batch_count)
+        time_epoch_stop = time.time()
+        self._writer_train.add_scalar('epoch', epoch, batch_count)
+        self._writer_train.add_scalar('time_s/epoch', self._get_seconds(time_epoch_start, time_epoch_stop), batch_count)
+        self._writer_train.add_scalar('time_s/total', self._get_seconds(time_start, time_epoch_stop), batch_count)
+        return batch_count, loss_train
+
+    def _train_batch(self, batch, batch_count):
+        time_batch_start = time.time()
+        batch_tokens, batch_actions = batch
+        _, batch_actions_lengths, _ = batch_actions
+        batch_log_probs = self._model(batch_tokens, batch_actions)
+        loss_train = self._loss(batch_log_probs, batch_actions_lengths)
+        self._optimize(loss_train)
+        time_batch_stop = time.time()
+        self._writer_train.add_scalar('loss', loss_train, batch_count)
+        self._writer_train.add_scalar('time_s/batch', self._get_seconds(time_batch_start, time_batch_stop), batch_count)
+        return loss_train
+
+    def _validate(self, batch_count):
+        time_val_start = time.time()
         self._model.eval()
         losses = []
         for tokens, actions in self._iterator_val:
@@ -50,5 +82,12 @@ class TrainTask(Task):
             loss = self._loss(log_probs, actions_lengths)
             losses.append(loss)
         loss_val = sum(losses) / len(losses)
+        time_val_stop = time.time()
+        self._writer_val.add_scalar('loss', loss_val, batch_count)
+        self._writer_val.add_scalar('time_s/val', self._get_seconds(time_val_start, time_val_stop), batch_count)
         self._model.train()
         return loss_val
+
+    def _get_seconds(self, start, stop):
+        seconds = stop - start
+        return seconds
