17258780761a5b0aec9aef4ef4950ce1e1201138
Christoffer Øhrstrøm <chrisnave@gmail.com>
2020-03-23 21:50:28 +0900

    Buffer-only and history-only representations.

---
+++ app/representations/history_only.py
@@ -0,0 +1,46 @@
+from app.representations.representation import Representation
+import torch
+from torch import nn
+
+class HistoryOnlyRepresentation(Representation):
+    """
+    History-only representation.
+    """
+
+    def __init__(self, embedding_size, representation_size, dropout):
+        """
+        :type embedding_size: int
+        :type representation_size: int
+        :type dropout: float
+        """
+        super().__init__()
+        self.representation_size = representation_size
+        self.feedforward = nn.Linear(in_features=embedding_size, out_features=representation_size, bias=True)
+        self.activation = nn.ReLU()
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, action_history, action_history_lengths, stack, stack_lengths, token_buffer, token_buffer_lengths):
+        """
+        :type action_history: torch.Tensor
+        :type action_history_lengths: torch.Tensor
+        :type stack: torch.Tensor
+        :type stack_lengths: torch.Tensor
+        :type token_buffer: torch.Tensor
+        :type token_buffer_lengths: torch.Tensor
+        :rtype: torch.Tensor
+        """
+        output = self.pick_last(action_history, action_history_lengths)
+        output = self.dropout(output)
+        output = self.feedforward(output)
+        output = self.activation(output)
+        return output
+
+    def pick_last(self, embeddings, lengths):
+        _, batch_size, hidden_size = embeddings.shape
+        last_index = lengths - 1
+        top = last_index.view(1, batch_size, 1).expand(1, batch_size, hidden_size)
+        last = torch.gather(embeddings, 0, top).view(1, batch_size, hidden_size)
+        return last
+
+    def __str__(self):
+        return f'HistoryOnly(size={self.representation_size})'
--- app/representations/selector.py
+++ app/representations/selector.py
@@ -4,6 +4,12 @@ def get_representation(rnn_input_size, rnn_hidden_size, config):
     :type rnn_hidden_size: int
     :type config: object
     """
+    if config.type == 'buffer_only':
+        from app.representations.buffer_only import BufferOnlyRepresentation
+        return BufferOnlyRepresentation(rnn_hidden_size, rnn_input_size, config.dropout)
+    elif config.type == 'history_only':
+        from app.representations.history_only import HistoryOnlyRepresentation
+        return HistoryOnlyRepresentation(rnn_hidden_size, rnn_input_size, config.dropout)
     if config.type == 'stack_only':
         from app.representations.stack_only import StackOnlyRepresentation
         return StackOnlyRepresentation(rnn_hidden_size, rnn_input_size, config.dropout)
