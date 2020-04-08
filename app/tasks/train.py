ba1c452a49735456e9393d7397d97f8b4dac0e1b
Christoffer Øhrstrøm <chrisnave@gmail.com>
2020-04-08 14:15:11 +0900

    Iteration numbers are denoted by sequence count in tensorboard.
    This used to be the batch count. Batch count was replaced with
    sequence count to make it easier to compare training across different
    batch sizes.

--- app/tasks/train.py
+++ app/tasks/train.py
@@ -54,27 +54,22 @@ class TrainTask(Task):
         self.evaluator = evaluator
         self.sampler = sampler
         self.log_train_every = log_train_every
-
         self.token_count = token_count
         self.tag_count = tag_count
         self.non_terminal_count = non_terminal_count
         self.action_count = action_count
-
         working_directory = os.getcwd()
         tensorboard_directory = os.path.join(working_directory, 'tb')
         train_directory = os.path.join(tensorboard_directory, 'train')
         val_directory = os.path.join(tensorboard_directory, 'val')
         self.writer_train = SummaryWriter(log_dir=train_directory)
         self.writer_val = SummaryWriter(log_dir=val_directory)
-
         self.total_time_offset = 0
         self.start_epoch = 0
         self.start_batch_count = 0
-
+        self.start_sequence_count = 0
         self.logger = logging.getLogger('train')
-
         self.uses_gpu = self.device.type == 'cuda'
-
         if load_checkpoint is not None:
             self.load_checkpoint(load_checkpoint)
 
@@ -90,25 +85,28 @@ class TrainTask(Task):
         self.logger.info(f'Parameters: {self.count_parameters():,}')
         self.logger.info(f'Model:\n{self.model}')
         batch_count = self.start_batch_count
+        sequence_count = self.start_sequence_count
         epoch = self.start_epoch
+        last_log_count = self.start_sequence_count
         log_values = []
         best_val_score = None
         self.model.train()
         if self.evaluator.should_evaluate(epoch, batch_count, pretraining=True):
-            best_val_score = self.evaluate(epoch, batch_count, best_val_score)
+            best_val_score = self.evaluate(epoch, batch_count, sequence_count, best_val_score)
         # assumes a single learning rate for all parameters
         learning_rate = self.optimizer.param_groups[0]['lr']
-        self.log_epoch(epoch, batch_count, learning_rate)
+        self.log_epoch(epoch, batch_count, sequence_count, learning_rate)
         while True:
-            epoch, batch_count, done, log_values, best_val_score = self.train_epoch(epoch, batch_count, log_values, best_val_score)
+            train_epoch_outputs = self.train_epoch(epoch, batch_count, sequence_count, last_log_count, log_values, best_val_score)
+            epoch, batch_count, sequence_count, last_log_count, done, log_values, best_val_score = train_epoch_outputs
             if done:
                 break
             if self.evaluator.should_evaluate(epoch, batch_count, end_of_epoch=True):
-                best_val_score = self.evaluate(epoch, batch_count, best_val_score)
+                best_val_score = self.evaluate(epoch, batch_count, sequence_count, best_val_score)
             self.stopping_criterion.add_epoch(epoch)
             if self.stopping_criterion.is_done():
                 break
-        self.evaluate(epoch, batch_count, best_val_score)
+        self.evaluate(epoch, batch_count, sequence_count, best_val_score)
         time_stop = time.time()
         time_seconds = time_stop - self.start_timestamp
         time_hours = time_seconds / 3600
@@ -118,22 +116,24 @@ class TrainTask(Task):
         self.writer_train.close()
         self.writer_val.close()
 
-    def train_epoch(self, epoch, batch_count, log_values, best_val_score):
+    def train_epoch(self, epoch, batch_count, sequence_count, last_log_count, log_values, best_val_score):
         time_epoch_start = time.time()
         start_of_epoch = True
         for batch in self.iterator_train:
             batch_count += 1
-            batch_outputs = self.train_batch(batch, batch_count)
+            sequence_count += batch.size
+            batch_outputs = self.train_batch(batch)
             log_values.append(batch_outputs)
-            if batch_count % self.log_train_every == 0:
-                self.log_train(log_values, batch_count)
+            if self.log_train_every <= sequence_count - last_log_count:
+                self.log_train(log_values, sequence_count)
+                last_log_count = sequence_count
                 log_values = []
             if self.evaluator.should_evaluate(epoch, batch_count):
-                best_val_score = self.evaluate(epoch, batch_count, best_val_score)
+                best_val_score = self.evaluate(epoch, batch_count, sequence_count, best_val_score)
             if self.stopping_criterion.is_done():
-                return epoch, batch_count, True, log_values, best_val_score
+                return epoch, batch_count, sequence_count, last_log_count, True, log_values, best_val_score
             if self.checkpoint.should_save_checkpoint(epoch, batch_count, start_of_epoch):
-                self.save_checkpoint(epoch, batch_count)
+                self.save_checkpoint(epoch, batch_count, sequence_count)
             start_of_epoch = False
         self.learning_rate_scheduler.step()
         # assumes a single learning rate for all parameters
@@ -142,12 +142,12 @@ class TrainTask(Task):
         time_epoch_stop = time.time()
         time_epoch = time_epoch_stop - time_epoch_start
         time_total = self.get_time_elapsed()
-        self.log_epoch(epoch, batch_count, learning_rate)
-        self.writer_train.add_scalar('time/epoch_s', time_epoch, batch_count)
-        self.writer_train.add_scalar('time/total_s', time_total, batch_count)
-        return epoch, batch_count, False, log_values, best_val_score
+        self.log_epoch(epoch, batch_count, sequence_count, learning_rate)
+        self.writer_train.add_scalar('time/epoch_s', time_epoch, sequence_count)
+        self.writer_train.add_scalar('time/total_s', time_total, sequence_count)
+        return epoch, batch_count, sequence_count, last_log_count, False, log_values, best_val_score
 
-    def train_batch(self, batch, batch_count):
+    def train_batch(self, batch):
         self.start_measure_memory()
         time_batch_start = time.time()
         batch_log_probs = self.model.batch_log_likelihood(batch)
@@ -174,7 +174,7 @@ class TrainTask(Task):
         loss.backward()
         self.optimizer.step()
 
-    def evaluate(self, epoch, batch_count, best_score):
+    def evaluate(self, epoch, batch_count, sequence_count, best_score):
         self.start_measure_memory()
         time_evaluate_start = time.time()
         self.model.eval()
@@ -196,35 +196,35 @@ class TrainTask(Task):
         tokens_per_second = total_tokens / time_val
         sentences_per_second = total_sentences / time_val
         loss_val = sum(losses) / len(losses)
-        self.writer_val.add_scalar('training/loss', loss_val, batch_count)
+        self.writer_val.add_scalar('training/loss', loss_val, sequence_count)
         self.stopping_criterion.add_val_loss(loss_val)
         memory = self.stop_measure_memory()
         if memory is not None:
             allocated_gb, reserved_gb = memory
-            self.writer_val.add_scalar('memory/allocated_gb', allocated_gb, batch_count)
-            self.writer_val.add_scalar('memory/reserved_gb', reserved_gb, batch_count)
-        self.writer_val.add_figure('gradients/all', gradients_all, batch_count)
-        self.writer_val.add_figure('gradients/compose_only', gradients_compose_only, batch_count)
-        self.writer_val.add_figure('gradients/exclude_representation', gradients_exclude_rep, batch_count)
-        self.writer_val.add_scalar('time/evaluate_s', time_evaluate, batch_count)
-        self.writer_val.add_scalar('time/val_s', time_val, batch_count)
-        self.writer_val.add_scalar('time/actions_per_s', actions_per_second, batch_count)
-        self.writer_val.add_scalar('time/tokens_per_s', tokens_per_second, batch_count)
-        self.writer_val.add_scalar('time/sentences_per_s', sentences_per_second, batch_count)
+            self.writer_val.add_scalar('memory/allocated_gb', allocated_gb, sequence_count)
+            self.writer_val.add_scalar('memory/reserved_gb', reserved_gb, sequence_count)
+        self.writer_val.add_figure('gradients/all', gradients_all, sequence_count)
+        self.writer_val.add_figure('gradients/compose_only', gradients_compose_only, sequence_count)
+        self.writer_val.add_figure('gradients/exclude_representation', gradients_exclude_rep, sequence_count)
+        self.writer_val.add_scalar('time/evaluate_s', time_evaluate, sequence_count)
+        self.writer_val.add_scalar('time/val_s', time_val, sequence_count)
+        self.writer_val.add_scalar('time/actions_per_s', actions_per_second, sequence_count)
+        self.writer_val.add_scalar('time/tokens_per_s', tokens_per_second, sequence_count)
+        self.writer_val.add_scalar('time/sentences_per_s', sentences_per_second, sequence_count)
         if self.sampler is not None:
-            self.writer_val.add_figure('tree/groundtruth', groundtruth_tree, batch_count)
-            self.writer_val.add_figure('tree/predicted', predicted_tree, batch_count)
-            self.writer_val.add_figure('actions/groundtruth', groundtruth_probs, batch_count)
-            self.writer_val.add_figure('actions/predicted', predicted_probs, batch_count)
-            self.writer_val.add_scalar('time/sample_s', time_sample, batch_count)
-            self.writer_val.add_scalar('training/f1', f1, batch_count)
-            self.writer_val.add_scalar('training/precision', precision, batch_count)
-            self.writer_val.add_scalar('training/recall', recall, batch_count)
-            self.logger.info(f'epoch={epoch}, batch={batch_count}, loss_val={loss_val:0.6f}, f1_val={f1:0.2f}')
+            self.writer_val.add_figure('tree/groundtruth', groundtruth_tree, sequence_count)
+            self.writer_val.add_figure('tree/predicted', predicted_tree, sequence_count)
+            self.writer_val.add_figure('actions/groundtruth', groundtruth_probs, sequence_count)
+            self.writer_val.add_figure('actions/predicted', predicted_probs, sequence_count)
+            self.writer_val.add_scalar('time/sample_s', time_sample, sequence_count)
+            self.writer_val.add_scalar('training/f1', f1, sequence_count)
+            self.writer_val.add_scalar('training/precision', precision, sequence_count)
+            self.writer_val.add_scalar('training/recall', recall, sequence_count)
+            self.logger.info(f'epoch={epoch}, batch={batch_count}, sequences={sequence_count}, loss_val={loss_val:0.6f}, f1_val={f1:0.2f}')
             score = f1
             is_new_best_score = best_score is None or best_score < score
         else:
-            self.logger.info(f'epoch={epoch}, batch={batch_count}, loss_val={loss_val:0.8f}')
+            self.logger.info(f'epoch={epoch}, batch={batch_count}, sequences={sequence_count}, loss_val={loss_val:0.8f}')
             score = loss_val
             is_new_best_score = best_score is None or score < best_score
         self.evaluator.evaluation_finished()
@@ -289,15 +289,16 @@ class TrainTask(Task):
         self.logger.info(f'Saving model at {file_path}')
         self.model.save(file_path)
 
-    def save_checkpoint(self, epoch, batch_count):
+    def save_checkpoint(self, epoch, batch_count, sequence_count):
         time_checkpoint_start = time.time()
         checkpoints_directory = os.path.join(os.getcwd(), 'checkpoints')
         os.makedirs(checkpoints_directory, exist_ok=True)
-        checkpoint_name = f'epoch_{epoch}_batch_{batch_count}.pt'
+        checkpoint_name = f'epoch_{epoch}_batch_{batch_count}_seq_{sequence_count}.pt'
         checkpoint_path = os.path.join(checkpoints_directory, checkpoint_name)
         self.logger.info(f'Saving checkpoint at {checkpoint_path}')
         checkpoint = {
             'batch_count': batch_count,
+            'sequence_count': sequence_count,
             'epoch': epoch,
             'learning_rate_scheduler': self.learning_rate_scheduler.state_dict(),
             'model': self.model.state_dict(),
@@ -308,13 +309,14 @@ class TrainTask(Task):
         torch.save(checkpoint, checkpoint_path)
         time_checkpoint_stop = time.time()
         time_checkpoint = time_checkpoint_stop - time_checkpoint_start
-        self.writer_train.add_scalar('time/checkpoint_s', time_checkpoint, batch_count)
+        self.writer_train.add_scalar('time/checkpoint_s', time_checkpoint, sequence_count)
 
     def load_checkpoint(self, path):
         absolute_path = hydra.utils.to_absolute_path(path)
         self.logger.info(f'Loading checkpoint from {absolute_path}')
         checkpoint = torch.load(absolute_path)
         self.start_batch_count = checkpoint['batch_count']
+        self.start_sequence_count = checkpoint['sequence_count']
         self.start_epoch = checkpoint['epoch']
         self.total_time_offset = checkpoint['time_elapsed']
         self.model.load_state_dict(checkpoint['model'])
@@ -322,10 +324,10 @@ class TrainTask(Task):
         self.learning_rate_scheduler.load_state_dict(checkpoint['learning_rate_scheduler'])
         self.stopping_criterion.load_state_dict(checkpoint['stopping_criterion'])
 
-    def log_epoch(self, epoch, batch_count, learning_rate):
-        self.logger.info(f'epoch={epoch}, batch={batch_count}, learning rate={learning_rate}')
-        self.writer_train.add_scalar('training/epoch', epoch, batch_count)
-        self.writer_train.add_scalar('training/learning_rate', learning_rate, batch_count)
+    def log_epoch(self, epoch, batch_count, sequence_count, learning_rate):
+        self.logger.info(f'epoch={epoch}, batch={batch_count}, sequences={sequence_count}, learning rate={learning_rate}')
+        self.writer_train.add_scalar('training/epoch', epoch, sequence_count)
+        self.writer_train.add_scalar('training/learning_rate', learning_rate, sequence_count)
 
     def count_parameters(self):
         parameters = filter(lambda p: p.requires_grad, self.model.parameters())
@@ -355,7 +357,7 @@ class TrainTask(Task):
     def get_time_elapsed(self):
         return self.total_time_offset + (time.time() - self.start_timestamp)
 
-    def log_train(self, values, batch_count):
+    def log_train(self, values, sequence_count):
         # return loss_scalar, memory, actions_per_second, tokens_per_second, sentences_per_second, time_batch, time_optimize
         loss = self.mean_from_tuple_index(values, 0)
         allocated_gb = self.mean_from_tuple_index(values, 1)
@@ -365,16 +367,16 @@ class TrainTask(Task):
         sentences_per_second = self.mean_from_tuple_index(values, 5)
         time_batch = self.mean_from_tuple_index(values, 6)
         time_optimize = self.mean_from_tuple_index(values, 7)
-        self.writer_train.add_scalar('training/loss', loss, batch_count)
-        self.writer_train.add_scalar('time/actions_per_s', actions_per_second, batch_count)
-        self.writer_train.add_scalar('time/tokens_per_s', tokens_per_second, batch_count)
-        self.writer_train.add_scalar('time/sentences_per_s', sentences_per_second, batch_count)
-        self.writer_train.add_scalar('time/batch_s', time_batch, batch_count)
-        self.writer_train.add_scalar('time/optimize_s', time_optimize, batch_count)
+        self.writer_train.add_scalar('training/loss', loss, sequence_count)
+        self.writer_train.add_scalar('time/actions_per_s', actions_per_second, sequence_count)
+        self.writer_train.add_scalar('time/tokens_per_s', tokens_per_second, sequence_count)
+        self.writer_train.add_scalar('time/sentences_per_s', sentences_per_second, sequence_count)
+        self.writer_train.add_scalar('time/batch_s', time_batch, sequence_count)
+        self.writer_train.add_scalar('time/optimize_s', time_optimize, sequence_count)
         if allocated_gb is not None:
-            self.writer_train.add_scalar('memory/allocated_gb', allocated_gb, batch_count)
+            self.writer_train.add_scalar('memory/allocated_gb', allocated_gb, sequence_count)
         if reserved_gb is not None:
-            self.writer_train.add_scalar('memory/reserved_gb', reserved_gb, batch_count)
+            self.writer_train.add_scalar('memory/reserved_gb', reserved_gb, sequence_count)
 
     def mean_from_tuple_index(self, values, index):
         if values[0][index] is None:
